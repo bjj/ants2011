833713c78ec78b910462b515c7e660c725aaf825
Ben Jackson <ben@ben.com>
2011-11-09 19:12:28 -0800

    Fix nits in gridbfs and maintain a direction pointer.

--- gridbfs.h
+++ gridbfs.h
@@ -12,16 +12,18 @@ class GridBfs : std::iterator<std::forward_iterator_tag, Location>
 public:
     GridBfs()
         : dist(-1)
+        , passable(0)
     { }
 
     template <typename T>
     GridBfs(const Grid<T> &grid, Passable &p, const Location &start)
         : dist(0)
-        , passable(p)
+        , passable(&p)
     {
         visited.init(grid);
+        visited.reset();
         q.push(start);
-        visited(start) = true;
+        visited(start) = 5; // "don't move"
         q.push(Location(-1, -1));
         enqueueAll();
     }
@@ -83,29 +85,33 @@ public:
         return dist;
     }
 
+    int direction() const
+    {
+        return visited(q.front()) - 1;
+    }
+
 protected:
-    void enqueue(int dr, int dc)
+    void enqueue(int dr, int dc, int dir)
     {
         int r = (q.front().row + dr + visited.rows) % visited.rows;
         int c = (q.front().col + dc + visited.cols) % visited.cols;
         const Location loc(r, c);
-        if (!visited(loc) && passable(loc)) {
+        if (!visited(loc) && (*passable)(loc)) {
             q.push(loc);
-            visited(loc) = true;
+            visited(loc) = dir + 1;
         }
     }
     void enqueueAll()
     {
-        const Location &here = q.front();
-        enqueue(-1, 0);
-        enqueue(0, 1);
-        enqueue(1, 0);
-        enqueue(0, -1);
+        enqueue(-1, 0, 2); // go back S
+        enqueue(0, 1, 3);  // go back W
+        enqueue(1, 0, 0);  // go back N
+        enqueue(0, -1, 1); // go back E
     }
     int dist;
-    Passable passable;
+    Passable *passable;
     std::queue<Location> q;
-    Grid<bool> visited;
+    Grid<char> visited;
 };
 
 #endif /* GRIDBFS_H_ */
