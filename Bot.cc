24b00f795e451c2e1d1b25f4f815637d802031ce
Ben Jackson <ben@ben.com>
2011-11-24 01:08:10 -0800

    Remove some state.v.info because it's very slow

--- Bot.cc
+++ Bot.cc
@@ -240,9 +240,11 @@ void Bot::makeMoves()
                 state.makeMove(move.loc, dir);
             busy(move.loc)--;
             busy(new_loc)++;
+#if 0 // this is slow
             state.v.info(move.loc, *move.why);
             if (angle != 0)
                 state.v.info(new_loc, "angle" + angle);
+#endif
             moved = true;
         } else {
             retry.push(move);
