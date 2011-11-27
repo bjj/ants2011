9edd4b62dae0274ed1bfaab0467d1ec2f1b92e4e
Ben Jackson <ben@ben.com>
2011-11-26 17:38:08 -0800

    Remove ant killing bonuses that lead to stupidity in the annealing

--- combat.cc
+++ combat.cc
@@ -391,12 +391,14 @@ void Bot::combatGroup(Move::close_queue &moves, set<Location> &sessile, const ve
             if (!square.isWater && !square.isFood) {
                 enemies.push_back(Combat::Enemy(dest, id));
                 enemies.back().bonus += min(250, state.grid(*it).stationary * 2);
+                /*
                 if (e_myHills(dest) < 6)
                     enemies.back().bonus *= 20;
                 else if (e_myHills(dest) < 12)
                     enemies.back().bonus *= 2;
                 if (e_attack(dest) < 5)
                     enemies.back().bonus *= 2;
+                */
             }
         }
     }
