6bea5ced43274e0ffb0adf10346d5b081d217085
Ben Jackson <ben@ben.com>
2011-11-26 17:38:29 -0800

    Slight combat bonus for exploring

--- combat.cc
+++ combat.cc
@@ -429,6 +429,9 @@ void Bot::combatGroup(Move::close_queue &moves, set<Location> &sessile, const ve
             if (e_enemies(dest) < e_enemies(*it))
                 ant.moves[d].bonus += 7;
 
+            if (e_explore(dest) < e_explore(*it))
+                ant.moves[d].bonus += 1;
+
             if (e_attack(dest) < e_attack(*it)) {
                 if (e_attack(dest) < 3)
                     ant.moves[d].bonus += 50;
